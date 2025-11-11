bfe003087d95fc526e743f86ebb4eb342d779d75
opinr <opinurdestiana@gmail.com>
2025-11-11 18:02:08 +0700

    pembaruan tampilan

---
+++ app/src/main/java/com/dicoding/gunungkerinci/Profile/ProfileLanguageActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.gunungkerinci.Profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.gunungkerinci.R
+
+class ProfileLanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_language)
+    }
+}
