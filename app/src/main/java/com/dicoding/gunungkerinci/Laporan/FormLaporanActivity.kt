e711d8e594201fe3c02d756cd6c91ee89a17a477
opinr <opinurdestiana@gmail.com>
2025-11-14 20:36:55 +0700

    pembaruan tampilan

---
+++ app/src/main/java/com/dicoding/gunungkerinci/Laporan/FormLaporanActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.gunungkerinci.Laporan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.gunungkerinci.R
+
+class FormLaporanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_laporan)
+    }
+}
