0b064a83907efee4bf4a82a1ead7dd8c105347c1
opinr <opinurdestiana@gmail.com>
2025-12-16 22:18:59 +0700

    pembaruan gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,13 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.13.1")
+        classpath("com.android.tools.build:gradle:8.13.2")
         classpath("com.google.gms:google-services:4.3.15")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.1" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
 }
